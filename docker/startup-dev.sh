1af9a06e548db109418a77b57bf667bc87593c30
wmax <maxweiss.1987@googlemail.com>
2025-12-01 23:44:51 +0100

    feat: add psql and pgadmin containers to dockerized dev environment

---
+++ docker/startup-dev.sh
@@ -0,0 +1,9 @@
+if [ ! -d "node_modules" ]; then
+    npm install
+fi
+
+# if [ ! -f ".env.local" ]; then
+    cp docker/docker-env .env.local
+# fi
+
+npm run dev
--- startup-dev.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -d "node_modules" ]; then
-    npm install
-fi
-
-npm run dev
